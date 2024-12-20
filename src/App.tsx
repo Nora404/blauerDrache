62811ddd38736b7ebc961d65d961eee324342982
ina-verena <144120729+ina-verena@users.noreply.github.com>
2024-12-20 20:29:40 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './index.css';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div id="header">Header</div>
+      <div id="content">
+        <div id="navi">
+          Navi <br /> <p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test
+        </div>
+        <div id="main">
+          Main<br /> <p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test
+        </div>
+        <div id="charakter">
+          Charakter <br /> <p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test<p>Test</p>Test
+        </div>
+      </div>
+      <div id="footer">Footer</div>
+    </>
+  )
+}
+
+export default App
