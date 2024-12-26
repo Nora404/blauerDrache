367313d5537f69e32c75b064332d3755ec57e672
ina-verena <144120729+ina-verena@users.noreply.github.com>
2024-12-26 18:50:46 +0100

    connect more

--- src/playground/lahtheim/center/church/ChurchNavi.tsx
+++ src/playground/lahtheim/center/church/ChurchNavi.tsx
@@ -11,10 +11,12 @@ const ChurchNavi: React.FC<ChurchNaviProps> = () => {
         <div>
             <Header>Kirche</Header>
             <p className='mb-1 text-left'>
-                <Link to="/courtyard">Zurück zum <ColoredLetter>Vorplatz</ColoredLetter> gehen</Link><br />
+                <Link to="/church">Um <ColoredLetter>Segen</ColoredLetter> bitten<br />
+                    (-10 Gold +10 Leben)</Link>
             </p>
             <p className='text-left'>
                 <Link to="/church"><ColoredLetter>Umgebung</ColoredLetter> untersuchen</Link><br />
+                <Link to="/courtyard">Zurück zum <ColoredLetter>Vorplatz</ColoredLetter> gehen</Link><br />
             </p>
         </div>
     );
--- src/playground/lahtheim/center/graveyard/GraveyardNavi.tsx
+++ src/playground/lahtheim/center/graveyard/GraveyardNavi.tsx
@@ -11,10 +11,12 @@ const GraveyardNavi: React.FC<GraveyardNaviProps> = () => {
         <div>
             <Header>Friedhof</Header>
             <p className='mb-1 text-left'>
-                <Link to="/courtyard">Zurück zum <ColoredLetter>Vorplatz</ColoredLetter> gehen</Link><br />
+                <Link to="/graveyard">Gegen <ColoredLetter>Zombies</ColoredLetter> Kämpfen<br />
+                    (-1 Runde)</Link><br />
             </p>
             <p className='text-left'>
-                <Link to="/start"><ColoredLetter>Umgebung</ColoredLetter> untersuchen</Link><br/>
+                <Link to="/graveyard"><ColoredLetter>Umgebung</ColoredLetter> untersuchen</Link><br />
+                <Link to="/courtyard">Zurück zum <ColoredLetter>Vorplatz</ColoredLetter> gehen</Link><br />
             </p>
         </div>
     );
