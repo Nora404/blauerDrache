33e4fa9366b283b7363aeca3dd7b78dc3fa57868
ina-verena <144120729+ina-verena@users.noreply.github.com>
2025-01-01 13:38:13 +0100

    transit grafik ans dynamic

--- src/playground/game/game/Transit.tsx
+++ src/playground/game/game/Transit.tsx
@@ -1,7 +1,11 @@
+//#region [imports]
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
-import { SYSTEM } from '../../../data/colorfullStrings';
+import BackAndNextbtn from '../../../layout/NavBtn/BackAndNextBtn';
+import './Transit.css'
+//#endregion
 
+//#region [prepare]
 type TransitProps = {
 };
 
@@ -15,7 +19,9 @@ const Transit: React.FC<TransitProps> = () => {
     const initialSteps = Number(steps) || 5;
 
     const [currentSteps, setCurrentSteps] = useState<number>(initialSteps);
+    //#endregion
 
+    //#region [events]
     useEffect(() => {
         if (currentSteps <= 0) {
             navigate(`/${target}`, { replace: true });
@@ -23,8 +29,10 @@ const Transit: React.FC<TransitProps> = () => {
         if (currentSteps >= initialSteps + 1) {
             navigate(`/${location}`, { replace: true });
         }
-    }, [steps, navigate, target]);
+    }, [currentSteps, target, location]);
+    //#endregion
 
+    //#region [handler]
     const handleGoForward = () => {
         setCurrentSteps(prev => prev - 1);
     };
@@ -32,7 +40,13 @@ const Transit: React.FC<TransitProps> = () => {
     const handleGoBack = () => {
         setCurrentSteps(prev => prev + 1);
     };
+    //#endregion
 
+    //#region [helpers]
+    const currentStepIndex = initialSteps - currentSteps;
+    //#endregion
+
+    //#region [jsx]
     return (
         <div>
             <h2>Von {from} nach {to}</h2>
@@ -40,23 +54,25 @@ const Transit: React.FC<TransitProps> = () => {
                 Es sind noch {currentSteps} Schritte bis zu deinem Ziel.
             </p>
 
-            <div>
-                ( )---( )---( )---( )---( )---( )
-            </div>
+            <div className="steps-container">
+                {Array.from({ length: initialSteps }).map((_, index) => (
+                    // ist das gleiche wie <></> erzeugt keinen HTML Knoten (Bessere Performance)
+                    <React.Fragment key={index}>
+                        <div className={`step ${index === currentStepIndex ? 'active' : ''}`}>
+                            {index === currentStepIndex ? '.' : ''}
+                        </div>
+                        {index < initialSteps - 1 && <div className="step-line"></div>}
+                    </React.Fragment>
+                ))}
+            </div><br />
 
             {currentSteps > 0 && (
-                <div className='flex-row max-width'>
-                    <div onClick={handleGoBack} style={{ width: "50%" }}>
-                        {SYSTEM.zurück}
-                    </div>
-                    <div onClick={handleGoForward} style={{ width: "50%" }}>
-                        {SYSTEM.weiter}
-                    </div>
-                </div>
+                <BackAndNextbtn onBack={handleGoBack} onNext={handleGoForward} />
             )}
             {currentSteps <= 0 && <p>Du hast dein Ziel erreicht ...</p>}
         </div>
     );
+    //#endregion
 };
 
 export default Transit;
