911bfcd2a1cdf31a3ff3f920f1477b7ff2e3f1e2
Ina-Verena Ludwig <Ina-Verena.Ludwig@nea-x.de>
2025-01-22 12:16:32 +0100

    update NothGate

--- src/playground/lahtheim/cityLimit/northGate/NorthGate.tsx
+++ src/playground/lahtheim/cityLimit/northGate/NorthGate.tsx
@@ -1,66 +1,83 @@
 //#region [imports]
-import React, { } from 'react';
-import { GradientText } from '../../../../utility/GradientText';
-import { CREATURE, PLACES } from '../../../../data/helper/colorfullStrings';
-import NpcTalk from '../../../../utility/NpcTalk';
-import ActionButton from '../../../../layout/ActionButtons/ActionButton';
-import { useNavigate } from 'react-router-dom';
-import { observer } from 'mobx-react-lite';
-import { useRootStore } from '../../../../store';
+import React from "react";
+import { GradientText } from "../../../../utility/GradientText";
+import { CREATURE, PLACES } from "../../../../data/helper/colorfullStrings";
+import NpcTalk from "../../../../utility/NpcTalk";
+import { useNavigate } from "react-router-dom";
+import { observer } from "mobx-react-lite";
+import { useRootStore } from "../../../../store";
+import { WeightedEvent } from "../../../../data/eventData";
+import { MainPlaceTemplate } from "../../../../layout/MainPlaceTemplate";
 //#endregion
 
 //#region [prepare]
 type NorthGateProps = {
-    title?: string;
-    onClick?: () => void;
+  title?: string;
+  onClick?: () => void;
 };
 
 const NorthGate: React.FC<NorthGateProps> = observer(() => {
-    const { gameTime, gameState } = useRootStore();
-    const navigate = useNavigate();
-    //#endregion
+  const { gameTime, gameState } = useRootStore();
+  const navigate = useNavigate();
 
-    //#region [handler]
-    const handleGuardian = () => {
-        navigate("/north-gate-guardian");
-    }
+  const possibleEvents: WeightedEvent[] = [];
 
-    const handlePlaque = () => {
-        navigate("/north-gate-plaque");
-    }
-    //#endregion
+  const dayDescription = (
+    <div className="text-left">
+      <p className="mb-1">
+        Du stehst vor den Toren von Lahtheim Es ist mitlerweile{" "}
+        {gameTime.data.gameDay}, du spürst die
+        {gameState.data.temperature}e Luft auf deinem Gesicht. Bevor du auch nur
+        einen Schritt gehen kannst, kommt ein kleines{" "}
+        <GradientText colors={["#CF388F", "#8839CF"]}>
+          geflügeltes Wesen
+        </GradientText>{" "}
+        zu dir. Mit großen Augen schaut es erwartungsvoll zu dir hoch. Im
+        nächsten Moment fängt es mit schriller Stimme an zu reden:
+      </p>
 
-    //#region [jsx]
-    return (
-        <div className='max-width'>
-            <h2>{PLACES.Nordtor}</h2>
+      <p className="mb-1">
+        <NpcTalk name="geflügeltesWesen">
+          „Willkommen in Lahtheim! Bist du hier, um vor den Bezwingern des{" "}
+          {CREATURE.grünerDrache} zu fliehen? In unserem Dorf ist Platz für
+          dich. Sei nett, dann darfst du vielleicht einmal den{" "}
+          {CREATURE.blauerDrache} besuchen.“
+        </NpcTalk>
+      </p>
 
-            <div className='text-left'>
-                <p className='mb-1'>
-                    Du stehst vor den Toren von Lahtheim Es ist mitlerweile {gameTime.data.gameDay}, du spürst die
-                    {gameState.data.temperature}e Luft auf deinem Gesicht. Bevor du auch nur einen Schritt gehen kannst,
-                    kommt ein kleines <GradientText colors={['#CF388F', '#8839CF']}>geflügeltes Wesen</GradientText> zu dir.
-                    Mit großen Augen schaut es erwartungsvoll zu dir hoch.
-                    Im nächsten Moment fängt es mit schriller Stimme an zu reden:
-                </p>
+      <p className="mb-1">
+        Ohne auf deine Antwort zu warten dreht sich das{" "}
+        <GradientText colors={["#CF388F", "#8839CF"]}>
+          geflügelte Wesen
+        </GradientText>{" "}
+        um und flattert durch den Torbogen Richtung Brunnen davon.
+      </p>
+    </div>
+  );
+  const dayButtons = [
+    {
+      label: "Die Wachen um Hilfe bitten",
+      onClick: () => navigate("north-gate-guardian"),
+    },
+    {
+      label: "Die Plakette des Erfolgs betrachten",
+      onClick: () => navigate("/north-gate-plaque"),
+    },
+  ];
+  //#endregion
 
-                <p className='mb-1'>
-                    <NpcTalk name="geflügeltesWesen">
-                        „Willkommen in Lahtheim! Bist du hier, um vor den Bezwingern des {CREATURE.grünerDrache} zu fliehen?
-                        In unserem Dorf ist Platz für dich. Sei nett, dann darfst du vielleicht einmal den {CREATURE.blauerDrache} besuchen.“
-                    </NpcTalk>
-                </p>
-
-                <p className='mb-1'>
-                    Ohne auf deine Antwort zu warten dreht sich das <GradientText colors={['#CF388F', '#8839CF']}>geflügelte Wesen</GradientText> um und flattert durch den Torbogen Richtung Brunnen davon.
-                </p><br />
-
-                <ActionButton onClick={handleGuardian} label='Die Wachen um Hilfe bitten' />
-                <ActionButton onClick={handlePlaque} label='Die Plakette des Erfolgs betrachten' />
-            </div>
-        </div>
-    );
-    //#endregion
+  //#region [jsx]
+  return (
+    <div className="max-width">
+      <MainPlaceTemplate
+        title={PLACES.Nordtor}
+        dayDescription={dayDescription}
+        dayButtons={dayButtons}
+        possibleEvents={possibleEvents}
+      />
+    </div>
+  );
+  //#endregion
 });
 
 export default NorthGate;
