0465e6b66b9f6baf84ea86c3c1a101bac99de5e0
ina-verena <144120729+ina-verena@users.noreply.github.com>
2025-02-16 11:04:57 +0100

    all place img

--- src/playground/game/infos/Map.tsx
+++ src/playground/game/infos/Map.tsx
@@ -13,8 +13,24 @@ import img07 from '../../../assets/img/marsh.webp';
 import img08 from '../../../assets/img/ork.webp';
 import img09 from '../../../assets/img/ruin.webp';
 
+import img10 from '../../../assets/img/edge.webp';
+import img11 from '../../../assets/img/forest.webp';
+import img12 from '../../../assets/img/sylvan.webp';
+import img13 from '../../../assets/img/deepForest.webp';
+import img14 from '../../../assets/img/fenril.webp';
 
+import img15 from '../../../assets/img/meadow.webp';
+import img16 from '../../../assets/img/steppe.webp';
+import img17 from '../../../assets/img/badlands.webp';
+import img18 from '../../../assets/img/echsen.webp';
 
+import img19 from '../../../assets/img/mountainFoot.webp';
+import img20 from '../../../assets/img/mountainPath.webp';
+import img21 from '../../../assets/img/mountanPick.webp';
+import img22 from '../../../assets/img/felkin.webp';
+import img23 from '../../../assets/img/zwerk.webp';
+
+import img24 from '../../../assets/img/darkLand.webp';
 
 const Map: React.FC = () => {
 
@@ -115,16 +131,151 @@ const Map: React.FC = () => {
             </div>
 
             <Header>Die Geisterwälder</Header>
-            <div></div>
+            <div>
+                <div className='flex-row mb-2'>
+                    <img src={img10} alt="Lahtheim" style={{ width: "150px" }} />
+                    <div className='text-left flex-top padding-x'>
+                        <b>Waldrand</b>
+                        <p>
+                            Die Hauptstadt Lahtheim ist ein Ort des Handels und der Kultur. Hier leben die meisten Menschen und es gibt viele Geschäfte und Tavernen.
+                        </p>
+                    </div>
+                </div>
+                <div className='flex-row mb-2'>
+                    <img src={img11} alt="Lahtheim" style={{ width: "150px" }} />
+                    <div className='text-left flex-top padding-x'>
+                        <b>Wald</b>
+                        <p>
+                            Die Hauptstadt Lahtheim ist ein Ort des Handels und der Kultur. Hier leben die meisten Menschen und es gibt viele Geschäfte und Tavernen.
+                        </p>
+                    </div>
+                </div>
+                <div className='flex-row mb-2'>
+                    <img src={img12} alt="Lahtheim" style={{ width: "150px" }} />
+                    <div className='text-left flex-top padding-x'>
+                        <b>Sylvanienwald</b>
+                        <p>
+                            Die Hauptstadt Lahtheim ist ein Ort des Handels und der Kultur. Hier leben die meisten Menschen und es gibt viele Geschäfte und Tavernen.
+                        </p>
+                    </div></div>
+                <div className='flex-row mb-2'>
+                    <img src={img13} alt="Lahtheim" style={{ width: "150px" }} />
+                    <div className='text-left flex-top padding-x'>
+                        <b>Tiefwald</b>
+                        <p>
+                            Die Hauptstadt Lahtheim ist ein Ort des Handels und der Kultur. Hier leben die meisten Menschen und es gibt viele Geschäfte und Tavernen.
+                        </p>
+                    </div></div>
+                <div className='flex-row mb-2'>
+                    <img src={img14} alt="Lahtheim" style={{ width: "150px" }} />
+                    <div className='text-left flex-top padding-x'>
+                        <b>Fenrilwald</b>
+                        <p>
+                            Die Hauptstadt Lahtheim ist ein Ort des Handels und der Kultur. Hier leben die meisten Menschen und es gibt viele Geschäfte und Tavernen.
+                        </p>
+                    </div>
+                </div>
+            </div>
 
             <Header>Im Osten</Header>
-            <div></div>
+            <div>
+                <div className='flex-row mb-2'>
+                    <img src={img15} alt="Lahtheim" style={{ width: "150px" }} />
+                    <div className='text-left flex-top padding-x'>
+                        <b>Wiese</b>
+                        <p>
+                            Die Hauptstadt Lahtheim ist ein Ort des Handels und der Kultur. Hier leben die meisten Menschen und es gibt viele Geschäfte und Tavernen.
+                        </p>
+                    </div>
+                </div>
+                <div className='flex-row mb-2'>
+                    <img src={img16} alt="Lahtheim" style={{ width: "150px" }} />
+                    <div className='text-left flex-top padding-x'>
+                        <b>Steppe</b>
+                        <p>
+                            Die Hauptstadt Lahtheim ist ein Ort des Handels und der Kultur. Hier leben die meisten Menschen und es gibt viele Geschäfte und Tavernen.
+                        </p>
+                    </div>
+                </div>
+                <div className='flex-row mb-2'>
+                    <img src={img17} alt="Lahtheim" style={{ width: "150px" }} />
+                    <div className='text-left flex-top padding-x'>
+                        <b>Ödland</b>
+                        <p>
+                            Die Hauptstadt Lahtheim ist ein Ort des Handels und der Kultur. Hier leben die meisten Menschen und es gibt viele Geschäfte und Tavernen.
+                        </p>
+                    </div></div>
+                <div className='flex-row mb-2'>
+                    <img src={img18} alt="Lahtheim" style={{ width: "150px" }} />
+                    <div className='text-left flex-top padding-x'>
+                        <b>Echsenland</b>
+                        <p>
+                            Die Hauptstadt Lahtheim ist ein Ort des Handels und der Kultur. Hier leben die meisten Menschen und es gibt viele Geschäfte und Tavernen.
+                        </p>
+                    </div></div>
+            </div>
 
             <Header>Gebirgskette Leerenwall</Header>
-            <div></div>
+            <div>
+                <div className='flex-row mb-2'>
+                    <img src={img19} alt="Lahtheim" style={{ width: "150px" }} />
+                    <div className='text-left flex-top padding-x'>
+                        <b>Bergfuß</b>
+                        <p>
+                            Die Hauptstadt Lahtheim ist ein Ort des Handels und der Kultur. Hier leben die meisten Menschen und es gibt viele Geschäfte und Tavernen.
+                        </p>
+                    </div>
+                </div>
+                <div className='flex-row mb-2'>
+                    <img src={img20} alt="Lahtheim" style={{ width: "150px" }} />
+                    <div className='text-left flex-top padding-x'>
+                        <b>Bergpfad</b>
+                        <p>
+                            Die Hauptstadt Lahtheim ist ein Ort des Handels und der Kultur. Hier leben die meisten Menschen und es gibt viele Geschäfte und Tavernen.
+                        </p>
+                    </div>
+                </div>
+                <div className='flex-row mb-2'>
+                    <img src={img21} alt="Lahtheim" style={{ width: "150px" }} />
+                    <div className='text-left flex-top padding-x'>
+                        <b>Berggipfel</b>
+                        <p>
+                            Die Hauptstadt Lahtheim ist ein Ort des Handels und der Kultur. Hier leben die meisten Menschen und es gibt viele Geschäfte und Tavernen.
+                        </p>
+                    </div>
+                </div>
+                <div className='flex-row mb-2'>
+                    <img src={img22} alt="Lahtheim" style={{ width: "150px" }} />
+                    <div className='text-left flex-top padding-x'>
+                        <b>Felkinland</b>
+                        <p>
+                            Die Hauptstadt Lahtheim ist ein Ort des Handels und der Kultur. Hier leben die meisten Menschen und es gibt viele Geschäfte und Tavernen.
+                        </p>
+                    </div>
+                </div>
+                <div className='flex-row mb-2'>
+                    <img src={img23} alt="Lahtheim" style={{ width: "150px" }} />
+                    <div className='text-left flex-top padding-x'>
+                        <b>Zwerkenland</b>
+                        <p>
+                            Die Hauptstadt Lahtheim ist ein Ort des Handels und der Kultur. Hier leben die meisten Menschen und es gibt viele Geschäfte und Tavernen.
+                        </p>
+                    </div>
+                </div>
+            </div>
 
             <Header>Alles und Nichts</Header>
-            <div></div>
+            <div>
+                <div className='flex-row mb-2'>
+                    <img src={img24} alt="Lahtheim" style={{ width: "150px" }} />
+                    <div className='text-left flex-top padding-x'>
+                        <b>Finsterland</b>
+                        <p>
+                            Die Hauptstadt Lahtheim ist ein Ort des Handels und der Kultur. Hier leben die meisten Menschen und es gibt viele Geschäfte und Tavernen.
+                        </p>
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
