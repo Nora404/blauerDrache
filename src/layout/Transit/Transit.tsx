ff472991bc6773849cd2c467c9acb6e71a999a45
Ina-Verena Ludwig <Ina-Verena.Ludwig@nea-x.de>
2025-03-07 11:58:09 +0100

    transit with manager

--- src/layout/Transit/Transit.tsx
+++ src/layout/Transit/Transit.tsx
@@ -2,163 +2,156 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { GradientText } from "../../utility/Formatted/GradientText";
-import {
-  getPlaceLabelFromRoute,
-  getPlaceNameFromRoute,
-} from "../../routings/mappingPathToLabel";
+import { getPlaceLabelFromRoute, getPlaceNameFromRoute } from "../../routings/mappingPathToLabel";
 import { getEventByPlace } from "../../utility/Event/TriggerEvent";
 import "./Transit.css";
 import TwoActionButton from "../ActionButtons/TwoActionButton";
-import { GameEventChain } from "../Events/GameEventChain";
+import { EventManager } from "../Events/EventManager";
+import { WeightedEvent } from "../../data/eventData";
 //#endregion
 
 //#region [prepare]
 const Transit: React.FC = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
+	const navigate = useNavigate();
+	const location = useLocation();
 
-  const { targetPath, startPath, steps } = useParams<{
-    targetPath: string; // z.B. "path"
-    startPath: string; // z.B. "north-gate"
-    steps: string;
-  }>();
+	const { targetPath, startPath, steps } = useParams<{
+		targetPath: string; // z.B. "path"
+		startPath: string; // z.B. "north-gate"
+		steps: string;
+	}>();
 
-  const initialSteps = Number(steps) || 5;
-  const [currentSteps, setCurrentSteps] = useState<number>(initialSteps);
-  const [eventChainActive, setEventChainActive] = useState<string | null>(null);
+	const initialSteps = Number(steps) || 5;
+	const [currentSteps, setCurrentSteps] = useState<number>(initialSteps);
+	const [eventChainActive, setEventChainActive] = useState<WeightedEvent[]>([]);
 
-  const isAtStart = currentSteps === initialSteps;
-  const isAtEnd = currentSteps === 1;
-  //#endregion
+	const isAtStart = currentSteps === initialSteps;
+	const isAtEnd = currentSteps === 1;
+	//#endregion
 
-  //#region [events]
-  useEffect(() => {
-    if (currentSteps <= 0) {
-      navigate(`/${targetPath}`, { replace: true });
-    }
+	//#region [events]
+	useEffect(() => {
+		if (currentSteps <= 0) {
+			navigate(`/${targetPath}`, { replace: true });
+		}
 
-    if (currentSteps >= initialSteps + 1) {
-      const state = location.state as { from?: string } | undefined;
-      if (state?.from) {
-        navigate(state.from, { replace: true });
-      } else {
-        navigate(`/${startPath}`, { replace: true });
-      }
-    }
-  }, [currentSteps, startPath, targetPath, location, initialSteps, navigate]);
-  //#endregion
+		if (currentSteps >= initialSteps + 1) {
+			const state = location.state as { from?: string } | undefined;
+			if (state?.from) {
+				navigate(state.from, { replace: true });
+			} else {
+				navigate(`/${startPath}`, { replace: true });
+			}
+		}
+	}, [currentSteps, startPath, targetPath, location, initialSteps, navigate]);
+	//#endregion
 
-  //#region [handler]
-  const handleGoForward = () => {
-    setCurrentSteps((prev) => prev - 1);
-    triggerPossibleEvent();
-  };
+	//#region [handler]
+	const handleGoForward = () => {
+		setCurrentSteps((prev) => prev - 1);
+		triggerPossibleEvent();
+	};
 
-  const handleGoBack = () => {
-    setCurrentSteps((prev) => prev + 1);
-    triggerPossibleEvent();
-  };
+	const handleGoBack = () => {
+		setCurrentSteps((prev) => prev + 1);
+		triggerPossibleEvent();
+	};
 
-  const handleFastForward = () => {
-    setCurrentSteps(1);
-  };
+	const handleFastForward = () => {
+		setCurrentSteps(1);
+	};
 
-  const handleFastBack = () => {
-    setCurrentSteps(initialSteps);
-  };
+	const handleFastBack = () => {
+		setCurrentSteps(initialSteps);
+	};
 
-  const handleFinishEventChain = () => {
-    setEventChainActive(null);
-  };
-  //#endregion
+	const handleFinishEventChain = () => {
+		setEventChainActive([]);
+	};
+	//#endregion
 
-  //#region [helpers]
-  const currentStepIndex = initialSteps - currentSteps;
+	//#region [helpers]
+	const currentStepIndex = initialSteps - currentSteps;
 
-  const triggerPossibleEvent = () => {
-    const placeName = getPlaceNameFromRoute(startPath);
-    const gameEvent = getEventByPlace(placeName);
-    if (gameEvent) {
-      setEventChainActive(gameEvent.id);
-    }
-  };
-  //#endregion
+	const triggerPossibleEvent = () => {
+		const placeName = getPlaceNameFromRoute(startPath);
+		const gameEvent = getEventByPlace(placeName);
+		if (gameEvent) {
+			const activeEvent: WeightedEvent = {
+				eventId: gameEvent.id,
+				probability: 100,
+			};
+			setEventChainActive([activeEvent]);
+		}
+	};
+	//#endregion
 
-  //#region [jsx]
-  return (
-    <div className="max-width">
-      <h2>
-        Von {getPlaceLabelFromRoute(startPath || "")} nach{" "}
-        {getPlaceLabelFromRoute(targetPath || "")}
-      </h2>
-      <p className="mb-1 text-left">
-        Du ziehst los, um den nächsten Ort zu bereisen. Du stellst fest das du
-        noch{" "}
-        <b>
-          <GradientText>{currentSteps}</GradientText>
-        </b>{" "}
-        Schritte brauchst um {getPlaceLabelFromRoute(targetPath || "")} zu
-        erreichen. Du kannst dich auch einfach umdrehen und zu{" "}
-        {getPlaceLabelFromRoute(startPath || "")} zurück gehen. Auf so einer
-        Reise könntest du wertvolles finden: Reichtümer, Wissen oder einen
-        qualvollen Tod. Du kannst auch schnell laufen, aber dann wird vermutlich
-        nichts auf deinem Weg passieren.
-      </p>
-      <br />
+	//#region [jsx]
+	return (
+		<div className="max-width">
+			<h2>
+				Von {getPlaceLabelFromRoute(startPath || "")} nach{" "}
+				{getPlaceLabelFromRoute(targetPath || "")}
+			</h2>
+			<p className="mb-1 text-left">
+				Du ziehst los, um den nächsten Ort zu bereisen. Du stellst fest das du noch{" "}
+				<b>
+					<GradientText>{currentSteps}</GradientText>
+				</b>{" "}
+				Schritte brauchst um {getPlaceLabelFromRoute(targetPath || "")} zu erreichen. Du kannst dich
+				auch einfach umdrehen und zu {getPlaceLabelFromRoute(startPath || "")} zurück gehen. Auf so
+				einer Reise könntest du wertvolles finden: Reichtümer, Wissen oder einen qualvollen Tod. Du
+				kannst auch schnell laufen, aber dann wird vermutlich nichts auf deinem Weg passieren.
+			</p>
+			<br />
 
-      <div className="steps-container">
-        {Array.from({ length: initialSteps }).map((_, index) => (
-          // ist das gleiche wie <></> erzeugt keinen HTML Knoten (Bessere Performance)
-          <React.Fragment key={index}>
-            <div
-              className={`step ${index === currentStepIndex ? "active" : ""}`}
-            >
-              {index === currentStepIndex ? "." : ""}
-            </div>
-            {index < initialSteps - 1 && <div className="step-line"></div>}
-          </React.Fragment>
-        ))}
-      </div>
-      <br />
-      <br />
+			<div className="steps-container">
+				{Array.from({ length: initialSteps }).map((_, index) => (
+					// ist das gleiche wie <></> erzeugt keinen HTML Knoten (Bessere Performance)
+					<React.Fragment key={index}>
+						<div className={`step ${index === currentStepIndex ? "active" : ""}`}>
+							{index === currentStepIndex ? "." : ""}
+						</div>
+						{index < initialSteps - 1 && <div className="step-line"></div>}
+					</React.Fragment>
+				))}
+			</div>
+			<br />
+			<br />
 
-      {currentSteps > 0 && !eventChainActive && (
-        <>
-          <TwoActionButton
-            onLeftAction={handleGoBack}
-            leftBtn={isAtStart ? "vorheriges Gebiet" : "zurück"}
-            onRightAction={handleGoForward}
-            rightBtn={isAtEnd ? "nächstes Gebiet" : "weiter"}
-          />
-          <TwoActionButton
-            onLeftAction={handleFastBack}
-            leftBtn="schnell zurück"
-            leftDisable={isAtStart}
-            onRightAction={handleFastForward}
-            rightBtn="schnell weiter"
-            rightDisable={isAtEnd}
-          />
-        </>
-      )}
-      {currentSteps <= 0 && <p>Du hast dein Ziel erreicht ...</p>}
-      <br />
-      {eventChainActive ? (
-        <GameEventChain
-          initialEventName={eventChainActive}
-          onFinishChain={handleFinishEventChain}
-        />
-      ) : (
-        <p className="mb-1 text-left">
-          Links von dir ist Umgebung, rechts von dir ist Umgebung – alles sieht
-          völlig normal und unauffällig aus. Es ist schon fast langweilig, wie
-          ereignislos die letzten Schritte waren. Du kannst deinen Weg unbeirrt
-          weiter fortsetzen.
-        </p>
-      )}
-      <br />
-    </div>
-  );
-  //#endregion
+			{currentSteps > 0 && eventChainActive.length < 1 && (
+				<>
+					<TwoActionButton
+						onLeftAction={handleGoBack}
+						leftBtn={isAtStart ? "vorheriges Gebiet" : "zurück"}
+						onRightAction={handleGoForward}
+						rightBtn={isAtEnd ? "nächstes Gebiet" : "weiter"}
+					/>
+					<TwoActionButton
+						onLeftAction={handleFastBack}
+						leftBtn="schnell zurück"
+						leftDisable={isAtStart}
+						onRightAction={handleFastForward}
+						rightBtn="schnell weiter"
+						rightDisable={isAtEnd}
+					/>
+				</>
+			)}
+			{currentSteps <= 0 && <p>Du hast dein Ziel erreicht ...</p>}
+			<br />
+			{eventChainActive ? (
+				<EventManager events={eventChainActive} onFinish={handleFinishEventChain} />
+			) : (
+				<p className="mb-1 text-left">
+					Links von dir ist Umgebung, rechts von dir ist Umgebung – alles sieht völlig normal und
+					unauffällig aus. Es ist schon fast langweilig, wie ereignislos die letzten Schritte waren.
+					Du kannst deinen Weg unbeirrt weiter fortsetzen.
+				</p>
+			)}
+			<br />
+		</div>
+	);
+	//#endregion
 };
 
 export default Transit;
