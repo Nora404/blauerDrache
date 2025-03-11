5c00df4b3440bd03d40b9beb8663c0ff089da298
Ina-Verena Ludwig <Ina-Verena.Ludwig@nea-x.de>
2025-03-11 09:13:50 +0100

    2 rows in editor

--- src/utility/Editor/EventEditor/Components/ComponentAndColorPicker.tsx
+++ src/utility/Editor/EventEditor/Components/ComponentAndColorPicker.tsx
@@ -74,179 +74,176 @@ const ComponentAndColorPicker: React.FC<ComponentAndColorPickerProps> = ({
 	// Render
 	return (
 		<div className="flex-row w-full">
-			{/* 3 Buttons: Gradient / Multi / ColoredText */}
-			<div>
-				<button
-					onClick={() => {
-						setSelectedComponent("GradientText");
-						setSelectedColor("");
-					}}
-					style={{
-						border: selectedComponent === "GradientText" ? "2px solid white" : "2px solid black",
-						background: "linear-gradient(to right, red, yellow, green)", // z.B. Demo-Farbverlauf
-						width: 30,
-						height: 30,
-						margin: 5,
-					}}
-				/>
-				<button
-					onClick={() => {
-						setSelectedComponent("MultiColoredLetters");
-						setSelectedColor("");
-					}}
-					style={{
-						border:
-							selectedComponent === "MultiColoredLetters" ? "2px solid white" : "2px solid black",
-						background:
-							"repeating-linear-gradient(45deg, red, red 5px, yellow 5px, yellow 10px, green 10px, green 15px)",
-						width: 30,
-						height: 30,
-						margin: 5,
-					}}
-				/>
-				<button
-					onClick={() => {
-						setSelectedComponent("ColoredText");
-						setSelectedColor("");
-					}}
-					style={{
-						border: selectedComponent === "ColoredText" ? "2px solid white" : "2px solid black",
-						backgroundColor: "red",
-						width: 30,
-						height: 30,
-						margin: 5,
-					}}
-				/>
-			</div>
-
-			{/* Jetzt die Farbauswahl – abhängig vom Komponententyp */}
-			{isGradientOrMulti && (
-				<div className="flex-row">
-					{paletteKeys.map((key) => {
-						const originalFirstColor = colorPalettes[key][0];
-						const originalLastColor = colorPalettes[key][colorPalettes[key].length - 1];
-
-						// Heller machen der ersten Farbe um 10%
-						const lighterColor = lighten(originalFirstColor, 30);
-
-						// Dunkler machen der zweiten Farbe um 10%
-						const darkerColor = darken(originalLastColor, 30);
-
-						return (
-							<button
-								key={key}
-								onClick={() => setSelectedColor(key)}
-								style={{
-									border: selectedColor === key ? "2px solid white" : "1px solid black",
-									borderRadius: "50%",
-									marginRight: 5,
-									width: 30,
-									height: 30,
-									background: `linear-gradient(to right, ${lighterColor}, ${darkerColor})`,
-									cursor: "pointer",
-								}}
-							/>
-						);
-					})}
-
-					{/* Custom-Button */}
+			<div className="flex-row-right">
+				<div className="grid-2">
 					<button
-						onClick={() => setSelectedColor("custom")}
+						onClick={() => {
+							setSelectedComponent("GradientText");
+							setSelectedColor("");
+						}}
 						style={{
-							border: selectedColor === "custom" ? "2px solid white" : "1px solid black",
-							borderRadius: "50%",
-							marginRight: 5,
+							border: selectedComponent === "GradientText" ? "2px solid white" : "2px solid black",
+							background: "linear-gradient(to right, red, yellow, green)", // z.B. Demo-Farbverlauf
 							width: 30,
 							height: 30,
-							background: "linear-gradient(45deg, red, yellow, green)",
-							cursor: "pointer",
-						}}></button>
-				</div>
-			)}
-
-			{isColoredText && (
-				<div className="flex-row">
-					<div className="flex-row-left">
-						<button
-							onClick={() => setSelectedFormat("normal")}
-							className={selectedFormat === "normal" ? "circle circleActiveBorder" : "circle"}>
-							n
-						</button>
-						<button
-							onClick={() => setSelectedFormat("bold")}
-							className={selectedFormat === "bold" ? "circle circleActiveBorder" : "circle"}>
-							<b>b</b>
-						</button>
-						<button
-							onClick={() => setSelectedFormat("italic")}
-							className={selectedFormat === "italic" ? "circle circleActiveBorder" : "circle"}>
-							<i>i</i>
-						</button>{" "}
-					</div>
-
-					{singleColorKeys.map((key) => {
-						const hex = textColors[key];
-						return (
-							<button
-								key={key}
-								onClick={() => setSelectedColor(key)}
-								style={{
-									border: selectedColor === key ? "2px solid white" : "1px solid black",
-									borderRadius: "50px",
-									marginRight: 5,
-									width: 30,
-									height: 30,
-									backgroundColor: hex,
-								}}
-							/>
-						);
-					})}
-
-					{/* Custom (einzelne Farbe) */}
-					<button
-						onClick={() => setSelectedColor("custom")}
-						style={{
-							border: selectedColor === "custom" ? "2px solid white" : "1px solid black",
-							borderRadius: "50px",
-							marginRight: 5,
-							width: 30,
-							height: 30,
-							background: "linear-gradient(45deg, red, yellow, green)",
-						}}></button>
-				</div>
-			)}
-
-			{/* Wenn "custom" ausgewählt, zeigen wir evtl. ein Farbwähler-UI */}
-			{selectedColor === "custom" && (
-				<div className="flex-row" style={{ marginLeft: 10 }}>
-					<input
-						className="color-picker"
-						type="color"
-						value={newColor}
-						onChange={(e) => setNewColor(e.target.value)}
+						}}
 					/>
-					<button onClick={handleAddCustomColor} style={{ width: "25px" }} className="greenBtn1">
-						+
-					</button>
-					<div className="flex-row">
-						{customColors.map((col, i) => (
-							<div
-								key={i}
-								onClick={() => handleRemoveCustomColor(col)} // Farbe entfernen bei Klick
-								style={{
-									width: 10,
-									height: 20,
-									margin: 5,
-									borderRadius: "5px",
-									backgroundColor: col,
-									border: "1px solid black",
-									cursor: "pointer",
-								}}
-							/>
-						))}
-					</div>
+					<button
+						onClick={() => {
+							setSelectedComponent("MultiColoredLetters");
+							setSelectedColor("");
+						}}
+						style={{
+							border:
+								selectedComponent === "MultiColoredLetters" ? "2px solid white" : "2px solid black",
+							background:
+								"repeating-linear-gradient(45deg, red, red 5px, yellow 5px, yellow 10px, green 10px, green 15px)",
+							width: 30,
+							height: 30,
+						}}
+					/>
+					<button
+						onClick={() => {
+							setSelectedComponent("ColoredText");
+							setSelectedColor("");
+						}}
+						style={{
+							border: selectedComponent === "ColoredText" ? "2px solid white" : "2px solid black",
+							backgroundColor: "red",
+							width: 30,
+							height: 30,
+						}}
+					/>
 				</div>
-			)}
+
+				{/* Jetzt die Farbauswahl – abhängig vom Komponententyp */}
+				{isGradientOrMulti && (
+					<div className="flex-row">
+						<div className="grid-7">
+							{paletteKeys.map((key) => {
+								const originalFirstColor = colorPalettes[key][0];
+								const originalLastColor = colorPalettes[key][colorPalettes[key].length - 1];
+
+								// Heller machen der ersten Farbe um 10%
+								const lighterColor = lighten(originalFirstColor, 30);
+
+								// Dunkler machen der zweiten Farbe um 10%
+								const darkerColor = darken(originalLastColor, 30);
+
+								return (
+									<button
+										key={key}
+										onClick={() => setSelectedColor(key)}
+										style={{
+											border: selectedColor === key ? "2px solid white" : "2px solid black",
+											borderRadius: "50%",
+											width: 30,
+											height: 30,
+											background: `linear-gradient(to right, ${lighterColor}, ${darkerColor})`,
+											cursor: "pointer",
+										}}
+									/>
+								);
+							})}
+						</div>
+
+						{/* Custom-Button */}
+						<button
+							onClick={() => setSelectedColor("custom")}
+							style={{
+								border: selectedColor === "custom" ? "2px solid white" : "2px solid black",
+								borderRadius: "50%",
+								width: 30,
+								height: 30,
+								background: "linear-gradient(45deg, red, yellow, green)",
+								cursor: "pointer",
+							}}></button>
+					</div>
+				)}
+
+				{isColoredText && (
+					<div className="flex-row">
+						<div className="grid-2">
+							<button
+								onClick={() => setSelectedFormat("normal")}
+								className={selectedFormat === "normal" ? "circle circleActiveBorder" : "circle"}>
+								n
+							</button>
+							<button
+								onClick={() => setSelectedFormat("bold")}
+								className={selectedFormat === "bold" ? "circle circleActiveBorder" : "circle"}>
+								<b>b</b>
+							</button>
+							<button
+								onClick={() => setSelectedFormat("italic")}
+								className={selectedFormat === "italic" ? "circle circleActiveBorder" : "circle"}>
+								<i>i</i>
+							</button>{" "}
+						</div>
+
+						<div className="grid-7">
+							{singleColorKeys.map((key) => {
+								const hex = textColors[key];
+								return (
+									<button
+										key={key}
+										onClick={() => setSelectedColor(key)}
+										style={{
+											border: selectedColor === key ? "2px solid white" : "2px solid black",
+											borderRadius: "50px",
+											width: 30,
+											height: 30,
+											backgroundColor: hex,
+										}}
+									/>
+								);
+							})}
+						</div>
+
+						{/* Custom (einzelne Farbe) */}
+						<button
+							onClick={() => setSelectedColor("custom")}
+							style={{
+								border: selectedColor === "custom" ? "2px solid white" : "1px solid black",
+								borderRadius: "50px",
+								width: 30,
+								height: 30,
+								background: "linear-gradient(45deg, red, yellow, green)",
+							}}></button>
+					</div>
+				)}
+
+				{/* Wenn "custom" ausgewählt, zeigen wir evtl. ein Farbwähler-UI */}
+				{selectedColor === "custom" && (
+					<div className="flex-row">
+						<input
+							className="color-picker"
+							type="color"
+							value={newColor}
+							onChange={(e) => setNewColor(e.target.value)}
+						/>
+						<button onClick={handleAddCustomColor} style={{ width: "25px" }} className="greenBtn1">
+							+
+						</button>
+						<div className="flex-row">
+							{customColors.map((col, i) => (
+								<div
+									key={i}
+									onClick={() => handleRemoveCustomColor(col)} // Farbe entfernen bei Klick
+									style={{
+										width: 10,
+										height: 20,
+										borderRadius: "5px",
+										backgroundColor: col,
+										border: "1px solid black",
+										cursor: "pointer",
+									}}
+								/>
+							))}
+						</div>
+					</div>
+				)}
+			</div>
 
 			{/* Hinzufügen-Button */}
 			<button
