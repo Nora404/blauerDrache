3e336abc74867f2c3ac12c603297f571c5e26e7c
Ina-Verena Ludwig <Ina-Verena.Ludwig@nea-x.de>
2025-03-12 14:07:01 +0100

    better questlog

--- src/playground/game/infos/QuestLog.tsx
+++ src/playground/game/infos/QuestLog.tsx
@@ -1,10 +1,5 @@
 // #region [imports]
-import {
-  emptyQuest,
-  getGameQuestById,
-  HaveItem,
-  Progress,
-} from "../../../data/questData";
+import { emptyQuest, getGameQuestById, HaveItem, Progress } from "../../../data/questData";
 import ActionButton from "../../../layout/ActionButtons/ActionButton";
 import { useCallback } from "react";
 import { observer } from "mobx-react-lite";
@@ -15,99 +10,92 @@ import { redColors } from "../../../data/helper/colorMappingData";
 
 // #region [prepare]
 const Questlog: React.FC = observer(() => {
-  const { playerQuest, gameState } = useRootStore();
-  // #endregion
+	const { playerQuest, gameState } = useRootStore();
+	// #endregion
 
-  // #region [handler]
-  const handleAbandon = useCallback(
-    (questId: string) => {
-      playerQuest.updateQuest(questId, true);
-    },
-    [playerQuest]
-  );
+	// #region [handler]
+	const handleAbandon = useCallback(
+		(questId: string) => {
+			playerQuest.updateQuest(questId, true);
+		},
+		[playerQuest]
+	);
 
-  const handleClick = () => {
-    console.log("Aktive: ", Object.entries(playerQuest.data.activeQuests));
-    console.log("Wartet: ", Object.entries(gameState.data.currentEventQueue));
-    console.log("Fertig: ", Object.keys(playerQuest.data.completedQuest));
-  };
-  // #endregion
+	const handleClick = () => {
+		console.log("Aktive: ", Object.entries(playerQuest.data.activeQuests));
+		console.log("Wartet: ", Object.entries(gameState.data.currentEventQueue));
+		console.log("Fertig: ", Object.keys(playerQuest.data.completedQuest));
+	};
+	// #endregion
 
-  // #region [jsx]
-  return (
-    <div className="max-width">
-      <h2>Deine Aufgaben</h2>
-      <p className="mb-1 text-left">
-        Du hast ein kleines Büchlein bei dir, doch anstatt Einkaufszettel oder Gedichte hältst du darin deine Aufgaben fest. Wer könnte sich schon all die Orte und Namen ohne Hilfsmittel merken? Zwischen den Seiten stecken die abgerissenen Zettel deiner Auftraggeber, sorgsam verwahrt, damit du keinen Auftrag aus den Augen verlierst.
-      </p>
-      <br />
-      {playerQuest.data.activeQuests ? (
-        <div className="flex-warp">
-          {Object.entries(playerQuest.data.activeQuests).map(
-            ([questId, currentProgress]) => {
-              const questObject = getGameQuestById(questId) || emptyQuest;
-              return (
-                <div key={questId} className="text-center questbox paper">
-                  <strong className="text-yellow">{questObject.label}</strong>
-                  <p className="mb-1">
-                    {parseDescription(questObject.description)}
-                  </p>
-                  <p className="mb-1">
-                    {parseDescription(questObject.reward)}
-                  </p>
-                  <p
-                    className={
-                      currentProgress.isDone ? "text-green" : "text-white"
-                    }
-                  >
-                    {renderTask(currentProgress)}
-                    <br />
-                  </p>
-                  <ActionButton
-                    onClick={() => {
-                      handleAbandon(questObject.id);
-                    }}
-                    color={redColors}
-                    bgColor="red"
-                    label="Aufgeben"
-                  />
-                </div>
-              );
-            }
-          )}
-        </div>
-      ) : (
-        <p>Keine aktiven Aufgaben</p>
-      )}
-      <br />
-      <ActionButton onClick={handleClick} label="Debug" />
-    </div>
-  );
-  // #endregion
+	// #region [jsx]
+	return (
+		<div className="max-width">
+			<h2>Deine Aufgaben</h2>
+			<p className="mb-1 text-left">
+				Du hast ein kleines Büchlein bei dir, doch anstatt Einkaufszettel oder Gedichte hältst du
+				darin deine Aufgaben fest. Wer könnte sich schon all die Orte und Namen ohne Hilfsmittel
+				merken? Zwischen den Seiten stecken die abgerissenen Zettel deiner Auftraggeber, sorgsam
+				verwahrt, damit du keinen Auftrag aus den Augen verlierst.
+			</p>
+			<br />
+			{playerQuest.data.activeQuests ? (
+				<div className="flex-warp">
+					{Object.entries(playerQuest.data.activeQuests).map(([questId, currentProgress]) => {
+						const questObject = getGameQuestById(questId) || emptyQuest;
+						return (
+							<div key={questId} className="text-center questbox paper flex-quest">
+								<div>
+									<strong className="text-yellow">{questObject.label}</strong>
+									<p className="mb-1">{parseDescription(questObject.description)}</p>
+									<p className="mb-1">{parseDescription(questObject.reward)}</p>
+								</div>
+								<div>
+									<p className={currentProgress.isDone ? "text-green" : "text-white"}>
+										{renderTask(currentProgress)}
+										<br />
+									</p>
+									<ActionButton
+										onClick={() => {
+											handleAbandon(questObject.id);
+										}}
+										color={redColors}
+										bgColor="red"
+										label="Aufgeben"
+									/>
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			) : (
+				<p>Keine aktiven Aufgaben</p>
+			)}
+			<br />
+			<ActionButton onClick={handleClick} label="Debug" />
+		</div>
+	);
+	// #endregion
 });
 
 export default Questlog;
 
 const renderTask = (quest: Progress) => {
-  switch (quest.type) {
-    case "Begegnung":
-      return (
-        <span>
-          Triff dich mit {quest.task.label ?? "der gesuchten Person"}.
-        </span>
-      );
-    case "Besorgen":
-      return (
-        <>
-          {quest.task.haveItem?.map((item: HaveItem) => (
-            <span key={item.item}>
-              Besorge {item.need}x {item.item}, du hast {item.count} besorgt.
-            </span>
-          ))}
-        </>
-      );
+	switch (quest.type) {
+		case "Begegnung":
+			return <span>Triff dich mit {quest.task.label ?? "der gesuchten Person"}.</span>;
+		case "Besorgen":
+			return (
+				<>
+					{quest.task.haveItem?.map((item: HaveItem) => (
+						<span key={item.item}>
+							Besorge {item.need}x {item.item}, du hast {item.count} besorgt.
+						</span>
+					))}
+				</>
+			);
 
-    default:
-      break;
-  }
+		default:
+			break;
+	}
 };
