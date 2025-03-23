34c2f02184d486ea4be355fa34ce7817f3c00889
ina-verena <144120729+ina-verena@users.noreply.github.com>
2025-03-23 19:04:21 +0100

    start with citymaps

--- src/playground/game/infos/Map.tsx
+++ src/playground/game/infos/Map.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import karte from "../../../assets/karte5.png";
 import lahtheim from "../../../assets/lahtheim.png";
+import fayloria from "../../../assets/fayloria.png";
+import tekron from "../../../assets/tekron.png";
+import paras from "../../../assets/paras.png";
 import Header from '../../../layout/Header/Header';
 
 const Map: React.FC = () => {
@@ -16,10 +19,31 @@ const Map: React.FC = () => {
                 alt="Karte"
             /><br /><br />
 
-            <Header>Lahtheim</Header>
+
             <img
                 src={lahtheim}
-                style={{ width: '100%', marginTop: '10px' }}
+                style={{ width: '100%', marginTop: '30px' }}
+                alt="Karte"
+            /><br /><br />
+
+
+            <img
+                src={fayloria}
+                style={{ width: '100%', marginTop: '30px' }}
+                alt="Karte"
+            /><br /><br />
+
+
+            <img
+                src={tekron}
+                style={{ width: '100%', marginTop: '30px' }}
+                alt="Karte"
+            /><br /><br />
+
+
+            <img
+                src={paras}
+                style={{ width: '100%', marginTop: '30px' }}
                 alt="Karte"
             /><br /><br />
 
